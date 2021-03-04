3b148f802ba881a8af49336b1c6103914723045f
Jack Huntington-Rainey <jack@jacks-mbp-2.myfiosgateway.com>
2021-03-04 15:55:35 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+/*----- constants -----*/
+const playerLookup = {
+  'null': 'emptySquare',
+  '1': 'whitePiece',
+  '-1': 'blackPiece'
+}
+
+
+
+/*----- app's state (variables) -----*/
+
+
+
+
+/*----- cached element references -----*/
+
+
+
+
+/*----- event listeners -----*/
+
+
+
+
+/*----- functions -----*/
