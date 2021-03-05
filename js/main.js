d0e9b415a24787eeebd0a04fc74fc40e94db809a
Jack Huntington-Rainey <jack@jacks-mbp-2.myfiosgateway.com>
2021-03-05 17:26:43 -0500

    Initialise state variables

--- js/main.js
+++ js/main.js
@@ -1,25 +1,70 @@
 /*----- constants -----*/
 const playerLookup = {
-  'null': 'emptySquare',
-  '1': 'whitePiece',
-  '-1': 'blackPiece'
+  'black': {
+    
+  },
+  'white': {
+
+  }
 }
-
-
 
 /*----- app's state (variables) -----*/
 
+let board, winner, turn;
 
-
+/*
+icebox
+let takenPiecesBlack, takenPiecesWhite, boardOrientation;
+*/
 
 /*----- cached element references -----*/
+const boardEl = document.getElementById('board');
+const squareEls = [...document.querySelectorAll('# board div')];
+const msgEl = document.getElementById('message-section');
+const replayBtn = document.querySelector('button');
 
-
-
-
+/*
+icebox
+const bTakenPiecesEl = document.getElementById('w-taken-pieces');
+const wTakenPiecesEl = document.getElementById('w-taken-pieces');
+*/
 /*----- event listeners -----*/
-
+boardEl.addEventListener('click', handlePieceSelection);
+boardEl.addEventListener('click', handlePiecePlacement);
 
 
 
 /*----- functions -----*/
+function init() {
+  board = [
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0
+  ];
+  turn = 1;
+  winner = null;
+  render();
+}
+
+
+
+init();
+
+
+
+function handlePieceSelection() {
+
+}
+
+function handlePiecePlacement() {
+
+}
+
+function render() {
+  
+}
