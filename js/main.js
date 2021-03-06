c7a1bb611c84f57ec5ef19e80d10ece92f8f8eca
Jack Huntington-Rainey <jack@jacks-mbp-2.myfiosgateway.com>
2021-03-05 20:24:15 -0500

    rewrite initialise state function

--- js/main.js
+++ js/main.js
@@ -1,15 +1,75 @@
 /*----- constants -----*/
 const playerLookup = {
-  'black': {
-    
+  b: {
+    '-1': {
+      imgURL: "url('../media/chess-piece-sprites/b-pieces/b-pawn')",
+      move: pawnMove()
+      // value: 1,
+    },
+    '-2': {
+      imgURL: "url('../media/chess-piece-sprites/b-pieces/b-bishop')",
+      move: bishopMove()
+      // value: 3,
+    },
+    '-3': {
+      imgURL: "url('../media/chess-piece-sprites/b-pieces/b-knight')",
+      move: knightMove()
+      // value: 3,
+    },
+    '-4': {
+      imgURL: "url('../media/chess-piece-sprites/b-pieces/b-rook')",
+      move: rookMove()
+      // value: 5,
+    },
+    '-5': {
+      imgURL: "url('../media/chess-piece-sprites/b-pieces/b-queen')",
+      move: queenMove()
+      // value: 9,
+    },
+    '-6': {
+      imgURL: "url('../media/chess-piece-sprites/b-pieces/b-king')",
+      move: kingMove()
+      // value: 50,
+    }
   },
-  'white': {
-
+  w: {
+    '1': {
+      '1': "url('../media/chess-piece-sprites/w-pieces/w-pawn')",
+      move: pawnMove()
+      // value: 1,
+    },
+    '2': {
+      imgURL: "url('../media/chess-piece-sprites/w-pieces/w-bishop')",
+      move: bishopMove()
+      // value: 3,
+    },
+    '3': {
+      imgURL: "url('../media/chess-piece-sprites/w-pieces/w-knight')",
+      move: knightMove()
+      // value: 3,
+    },
+    '4': {
+      imgURL: "url('../media/chess-piece-sprites/w-pieces/w-rook')",
+      move: rookMove()
+      // value: 5,
+    },
+    '5': {
+      imgURL: "url('../media/chess-piece-sprites/w-pieces/w-queen')",
+      move: queenMove()
+      // value: 9,
+    },
+    '6': {
+      imgURL: "url('../media/chess-piece-sprites/w-pieces/w-king')",
+      move: kingMove()
+      // value: 50,
+    }
+  },
+  'null': {
+    imgURL: "url('')"
   }
 }
 
 /*----- app's state (variables) -----*/
-
 let board, winner, turn;
 
 /*
@@ -19,7 +79,7 @@ let takenPiecesBlack, takenPiecesWhite, boardOrientation;
 
 /*----- cached element references -----*/
 const boardEl = document.getElementById('board');
-const squareEls = [...document.querySelectorAll('# board div')];
+const squareEls = [...document.querySelectorAll('#board div')];
 const msgEl = document.getElementById('message-section');
 const replayBtn = document.querySelector('button');
 
@@ -31,31 +91,32 @@ const wTakenPiecesEl = document.getElementById('w-taken-pieces');
 /*----- event listeners -----*/
 boardEl.addEventListener('click', handlePieceSelection);
 boardEl.addEventListener('click', handlePiecePlacement);
-
-
+replayBtn.addEventListener('click', init);
 
 /*----- functions -----*/
 function init() {
   board = [
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0
+    -4, -3, -2, -6, -5, -2, -3, -4,
+    -1, -1, -1, -1, -1, -1, -1, -1,
+    null, null, null, null, null, null, null, null,
+    null, null, null, null, null, null, null, null,
+    null, null, null, null, null, null, null, null,
+    null, null, null, null, null, null, null, null,
+    1, 1, 1, 1, 1, 1, 1, 1,
+    4, 3, 2, 6, 5, 2, 3, 4
   ];
+  // Not sure whether or not to either have the board tracking the player or different pieces set to different values or even one kind of piece to one value whether it's black or white.
   turn = 1;
   winner = null;
   render();
 }
 
-
-
 init();
 
+function render() {
 
+  replayBtn.style.visibility = winner ? 'visible' : 'hidden';
+}
 
 function handlePieceSelection() {
 
@@ -65,6 +126,27 @@ function handlePiecePlacement() {
 
 }
 
-function render() {
-  
+function pawnMove() {
+
 }
+ 
+function bishopMove() {
+
+}
+ 
+function knightMove() {
+
+}
+ 
+function rookMove() {
+
+}
+ 
+function queenMove() {
+
+}
+ 
+function kingMove() {
+
+}
+ 
