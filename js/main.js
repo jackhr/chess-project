eab7d52ea7659445cc1ba087369e724b10027b83
Jack Huntington-Rainey <jack@jacks-mbp-2.myfiosgateway.com>
2021-03-11 17:56:32 -0500

    final final flourish

--- js/main.js
+++ js/main.js
@@ -149,6 +149,7 @@ function init() {
 }
 init();
 function render() {
+  // 
   board.forEach(function(squareValue, boardIdx) {
     const square = document.getElementById(`sq${boardIdx}`);
     square.style.backgroundImage = playerLookup[squareValue]['imgURL'];
